8535ee397dcae225aeede78a74310a6723598747
Sanjar <theesanjar@gmail.com>
2025-05-12 14:25:45 +0500

    made editquestionpage lazy loading

--- src/pages/EditQuestionPage/EditQuestionPage.jsx
+++ src/pages/EditQuestionPage/EditQuestionPage.jsx
@@ -5,7 +5,7 @@ import { useParams } from "react-router-dom";
 import { Loader } from "../../components/Loader";
 import { EditQuestion } from "./EditQuestion";
 
-export const EditQuestionPage = () => {
+const EditQuestionPage = () => {
   const { id } = useParams();
   const [question, setQuestion] = useState(null);
 
@@ -27,3 +27,5 @@ export const EditQuestionPage = () => {
     </>
   );
 };
+
+export default EditQuestionPage;
